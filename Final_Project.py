16a5e8b3a75d5483299595ca5a0ce519a9d2b847
Fadli99 <44110432+Fadli99@users.noreply.github.com>
2023-05-16 08:51:06 +0700

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
-import seaborn as sns
+#import seaborn as sns
 import streamlit as st
 from babel.numbers import format_currency
 sns.set(style='dark')
